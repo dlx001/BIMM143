b4816cf6aaf743704e661e246dcbaf8a0e0d90cb
DX <danielxu.314@gmail.com>
2022-11-17 10:31:28 -0800

    added class projects

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BIMM143
 Bioinformatics class
+
+This repo is for my UCSD bioinformatics class
---
+++ class04/class04.R
@@ -0,0 +1,7 @@
+#This is a comment
+x<-1:50
+plot(x, sin(x))
+
+#Add more input arguments
+plot(x, sin(x), type="l", col="blue", lwd=3)
+
